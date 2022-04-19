3a2052b3277fb1b4574a8664ba0503f243f5a3db
Lauris Mikāls <lauris.mikals@lokalise.com>
2022-04-19 15:35:11 +0300

    Return action to its original state

--- dist/index.js
+++ dist/index.js
@@ -5977,11 +5977,6 @@ async function run() {
     const accessToken = core.getInput('access-token');
 
     const endpoint = org ? `/orgs/${org}/repos` : '/user/repos'
-
-    core.info(`Name: ${name}`);
-    core.info(`Org: ${org}`);
-    core.info(`Endpoint: ${endpoint}`);
-
     axios.post(
       'https://api.github.com' + endpoint,
       {
@@ -5997,9 +5992,9 @@ async function run() {
     ).then((repository) => {
       core.info('Repository created: ' + repository.data.html_url);
       core.setOutput('id', repository.data.node_id);
-    }).catch((error) => {
+    }).catch(() => {
+      core.info('Repository already exists.');
       core.setOutput('id', null);
-      core.setFailed(error);
     })
   } catch (error) {
     core.setFailed(error.message);
